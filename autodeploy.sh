fff8797316b7233653a7157c25d315de8a54dfa4
aheneel <aheneel@gmail.com>
2025-02-06 00:53:18 +0200

    Update autodeploy.sh

--- autodeploy.sh
+++ autodeploy.sh
@@ -3,26 +3,41 @@
 # Останавливаем выполнение, если любая из команд вернет ошибку
 set -e
 
-# Устанавливаем необходимые пакеты
+# 1. Устанавливаем необходимые пакеты
 sudo apt-get update
 sudo apt-get install -y unzip golang
 
-# Скачиваем архив
+# 2. Скачиваем архив
 sudo wget -O /root/autodeploy-nginx-site-main.zip \
   https://github.com/aheneel/autodeploy-nginx-site/archive/refs/heads/main.zip
 
-# Распаковываем
+# 3. Распаковываем архив в /root
 sudo unzip /root/autodeploy-nginx-site-main.zip -d /root
 
-# Создаем папку install и переносим файлы
+# 4. Создаем папку install и переносим файлы
 sudo mkdir -p /root/install
 sudo mv /root/autodeploy-nginx-site-main/* /root/install/
 
-# Даем права на исполнение скриптам
-sudo chmod +x /root/install/*
-
-# Переходим в install и запускаем скрипты
+# 5. Переходим в /root/install
 cd /root/install
+
+# 6. Компилируем все .go файлы (1.go, 2.go, 3.go, 4.go, 5.go, 6.go, autodeploy.go)
+#    При компиляции go build <file.go> получится бинарник с таким же именем без .go
+sudo go build 1.go
+sudo go build 2.go
+sudo go build 3.go
+sudo go build 4.go
+sudo go build 5.go
+sudo go build 6.go
+sudo go build autodeploy.go
+
+# 7. Удаляем исходники .go
+sudo rm 1.go 2.go 3.go 4.go 5.go 6.go autodeploy.go
+
+# 8. Даем права на исполнение
+sudo chmod +x 1 2 3 4 5 6 autodeploy
+
+# 9. Запускаем нужные бинарники (1,2,3,4,5,6). autodeploy не запускаем
 sudo ./1
 sudo ./2
 sudo ./3
